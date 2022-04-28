579ca5157ed41cd313c9cba66bea68ee3c3b7e40
shAtud <sidalihouda.computerscience@gmail.com>
2022-04-28 21:40:46 +0100

    student cards commit

--- components/CardsStudent.js
+++ components/CardsStudent.js
@@ -5,6 +5,7 @@ const CardsStudent = props => {
         {
             id : 1,
             title : 'Equipes :' ,
+            url : "teamStudent.jpg",
             composants : [
                 {
                     composant : 'Complete :',
@@ -19,6 +20,7 @@ const CardsStudent = props => {
         {
             id : 1,
             title : 'Etudiants :' ,
+            url : "stdStudent.jpg",
             composants : [
                 {
                     composant : 'Sans équipe :',
@@ -33,6 +35,7 @@ const CardsStudent = props => {
         {
             id : 1,
             title : 'Thème :' ,
+            url : "themeStudent.jpg",
             composants : [
                 {
                     composant : 'Nombre total de thèmes :',
@@ -43,6 +46,7 @@ const CardsStudent = props => {
         {
             id : 1,
             title : 'Invitation :' ,
+            url : "joinStudent.jpg",
             composants : [
                 {
                     composant : 'Invitations envoyées :',
@@ -56,23 +60,30 @@ const CardsStudent = props => {
         },
     ]
     return (
-        <div className="lg:h-[100vh] p-[80px] flex flex-row items-center justify-center gap-12">
+        <div className="lg:h-[100vh] p-[80px] flex flex-row items-center justify-center gap-12 font-xyz">
             {
                 data.map((el,index)=> {
                     return(
-                        <div className="h-[300px] w-[250px] flex flex-col justify-center border-2 rounded-lg border-zinc-600">
-                            <div>{el.title}</div>
-                            <div>{el.composants.map((element) =>{
-                                return(
-                                    <div className="flex flex-row space-x-2">
-                                        <div>{element.composant}</div>
-                                        <div>{element.nombre}</div>
-                                    </div>
-                                )
-                            })}</div>
+                        <div className="h-[300px] hover:h-[310px] hover:w-[260px] w-[250px] flex flex-col items-center bg-gradient-to-b hover:from-slate-400 hover:to-slate-600 from-slate-300 to-slate-500  border-2 rounded-lg border-zinc-600 relative text-gray-900">
+                            <div className="  h-full w-full"> 
+                                <img src = {el.url} className="object-contain h-full w-full mix-blend-darken opacity-50"/>
+                            </div>
+                            <div className="absolute space-y-6 mt-6 flex items-center justify-center flex-col">
+                                <div className="text-[25px]">{el.title}</div>
+                                <div>{el.composants.map((element) =>{
+                                    return(
+                                        <div className="flex flex-row space-x-1">
+                                            <div className="text-[20px] truncate max-w-[220px]">{element.composant}</div>
+                                            <div className="text-[20px]">{element.nombre}</div>
+                                        </div>
+                                    )
+                                })}</div>
+                                <button className="h-[35px] w-[130px] text-[20px] bg-blue-300 hover:bg-blue-400 rounded-full">Voir plus</button>
+                            </div>
                         </div>
                     )
-                })
+                }
+                )
             }
         </div>
     )
