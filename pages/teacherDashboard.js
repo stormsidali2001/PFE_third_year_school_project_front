481417d3b711cc0c6d0a45096bbca5ed4d336ef8
shAtud <sidalihouda.computerscience@gmail.com>
2022-05-01 13:41:44 +0100

    acceuil teacher

---
+++ pages/teacherDashboard.js
@@ -0,0 +1,12 @@
+import AcceuilTeacher from "../components/AcceuilTeacher";
+import HorisontalNavbar from "../components/HorisontalNavbar";
+
+const teacherDashboard = props => {
+    return (
+        <div>
+            <HorisontalNavbar/>
+            <AcceuilTeacher/>
+        </div>
+    )
+}
+export default teacherDashboard;
