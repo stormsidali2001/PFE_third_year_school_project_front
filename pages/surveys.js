d502537f7be66741f2f93bd905d688dcbbecc94d
shAtud <sidalihouda.computerScience@gmail.com>
2022-05-08 18:17:45 +0200

    ...

--- pages/surveys.js
+++ pages/surveys.js
@@ -9,19 +9,8 @@ const AfficherTousLesSondages = props => {
         getSurveysThunk();
     },[])
     
-    const DayToHourMinSec = (temps) => {
-        
-        const [restant , setRestant] = useState(temps * 24 * 60 * 60) ;
-        const day = (restant / 86400)
-        const hours = (restant / (3600 * day) ) % 24
-        const min = (restant / (day * 24 *60)) % 60
-        const seconds = (restant / (day * 86400) -1 ) // le moin 1 est du au resultat de la division
-        const leftTime = day + " : " + hours + " : " + min  +" : " + seconds
-        return (
-          leftTime
-        )
-    }
-    console.log(DayToHourMinSec(25))
+    
+   
     return(
         <div className="h-fit min-h-screen py-16 w-[100vw] flex flex-row flex-wrap gap-12 items-center justify-center bg-blue-100 font-xyz">
             {
@@ -29,7 +18,10 @@ const AfficherTousLesSondages = props => {
                 ${el.tempsRestant == 0 ? "bg-gradient-to-b hover:from-blue-400 hover:to-blue-600 from-blue-300 to-blue-500" : "bg-gradient-to-b from-blue-100 to-blue-300 hover:from-blue-200 hover:to-blue-400"}
             */
                 surveys.map((el,index)=>{
-                let tempsRestant = new Date(el.createdAt).getTime() - Date.now();
+                let tempsRestant = new Date(el.createdAt).getTime() + el.period - Date.now();
+
+                const formatedRemainingTime = new Date(tempsRestant).getDate() + "J:" + new Date(tempsRestant).getHours() + ":" + new Date(tempsRestant).getMinutes() + ":" + new Date(tempsRestant).getSeconds() ;
+                console.log(tempsRestant,"_____",el.title)
                 if(tempsRestant < 0){
                     tempsRestant = 0;
                 }
@@ -43,7 +35,7 @@ const AfficherTousLesSondages = props => {
                             <div className="text-[18px] break-words border-2 w-full">{el.description}</div>
                             <div className="text-[22px] border-red-700">
                                 {
-                                tempsRestant == 0 ? "Sondage fermé" : "temps restant :"} {tempsRestant == 0 ? "" : `${DayToHourMinSec(tempsRestant)}`
+                                tempsRestant == 0 ? "Sondage fermé" : "temps restant :"} {tempsRestant == 0 ? "" : `${formatedRemainingTime}`
                                 }
                             </div>
                             {/* <div className="text-[22px] hidden">Options :</div> */}
