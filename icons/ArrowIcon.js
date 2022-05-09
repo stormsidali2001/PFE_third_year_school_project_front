6bcb2110f7fa72f5c8792879f14f9740c9752816
shAtud <sidalihouda.computerscience@gmail.com>
2022-05-09 18:54:31 +0100

    fin de ajouter étudiant et enseignant

---
+++ icons/ArrowIcon.js
@@ -0,0 +1,28 @@
+const ArrowIcon = props => {
+    return (
+        <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_524_2)">
+<g filter="url(#filter0_d_524_2)">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M19.4136 33.4138C19.0385 33.7888 18.5299 33.9994 17.9996 33.9994C17.4692 33.9994 16.9606 33.7888 16.5856 33.4138L4.58557 21.4138C4.21063 21.0388 4 20.5302 4 19.9998C4 19.4695 4.21063 18.9609 4.58557 18.5858L16.5856 6.58584C16.9628 6.22152 17.468 6.01993 17.9924 6.02449C18.5168 6.02905 19.0184 6.23938 19.3892 6.6102C19.76 6.98102 19.9704 7.48264 19.9749 8.00704C19.9795 8.53143 19.7779 9.03663 19.4136 9.41384L10.8276 17.9998H33.9996C34.53 17.9998 35.0387 18.2106 35.4138 18.5856C35.7889 18.9607 35.9996 19.4694 35.9996 19.9998C35.9996 20.5303 35.7889 21.039 35.4138 21.4141C35.0387 21.7891 34.53 21.9998 33.9996 21.9998H10.8276L19.4136 30.5858C19.7885 30.9609 19.9991 31.4695 19.9991 31.9998C19.9991 32.5302 19.7885 33.0388 19.4136 33.4138V33.4138Z" fill="#03045E"/>
+</g>
+</g>
+<defs>
+<filter id="filter0_d_524_2" x="0" y="6.02441" width="40" height="35.9746" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_524_2"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_524_2" result="shape"/>
+</filter>
+<clipPath id="clip0_524_2">
+<rect width="40" height="40" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+
+    )
+}
+export default ArrowIcon
---
+++ pages/addstudent1.js
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import HorisontalNavbar from "../components/HorisontalNavbar";
+import StudentVerticalNavbar from "../components/StudentVerticalNavbar";
+import ArrowIcon from "../icons/ArrowIcon";
+import AttachFileIcon from "../icons/AttachFileIcon";
+
+const addStudent = ({toastsRef}) => {
+    const [oneclick , setOneClick] = useState(false)
+    const [manyClick , setManyClick] = useState(true)
+    const [matricule , setMatricule] = useState("")
+    const [email , setEmail] = useState("")
+    const [firstName , setFirstName] = useState("")
+    const [lastName , setLastName] = useState("")
+    const [promotion , setPromotion] = useState("")
+    const [file,setFile] = useState({})
+    const extension = "csv"
+    return (
+       <div>
+           <StudentVerticalNavbar/>
+           <HorisontalNavbar/>
+            <div className="bg-background h-screen w-screen relative flex items-center justify-center font-xyz text-textcolor">
+                <img src="addStudent.jpg" className="h-full w-full object-contain mix-blend-darken absolute"/>
+                <div className={`h-[200px] w-[450px] bg-white/70 backdrop-blur-sm shadow-lg rounded-xl flex-col space-y-6 items-center justify-center text-[18px] ${oneclick || manyClick === true ? "hidden" : "flex"}`}>
+                    <div className="text-[23px] text-center px-10">Vous voulez ajouter un ou plusieur étudiant ?</div>
+                    <div className="space-x-6">
+                        <button className="h-[35px] w-[160px] rounded-full bg-[#32AFF5] text-white" onClick={()=> setOneClick(true)}>Un seul</button>
+                        <button className="h-[35px] w-[160px] rounded-full bg-[#8FD4FB]" onClick={()=> setManyClick(true)}>Plusieurs</button>
+                    </div>
+                </div>
+                <form className = {`h-[550px] w-[650px] bg-white/70 backdrop-blur-sm shadow-lg rounded-xl flex-col space-y-10 items-center justify-center relative text-[23px] ${oneclick === true ? "flex" : "hidden"}`}>
+                    <div className="text-[35px]">Ajouter un étudiant</div>
+                    <table>
+                        <tr className="">
+                            <td className="py-2">Matricule :</td>
+                            <td>
+                                <input className="h-[40px] w-[230px] rounded-lg bg-white/10 shadow-md backdrop-blur-sm outline-none px-3 text-[18px] font-thin" placeholder="Matricule..." onChange={(e)=>{setMatricule(e.target.value)}}/>
+                            </td>
+                        </tr>
+                        <tr className="">
+                            <td className="py-2">Email :</td>
+                            <td>
+                                <input className="h-[40px] w-[230px] rounded-lg bg-white/10 backdrop-blur-sm shadow-md outline-none px-3 text-[18px] font-thin" placeholder="E-mail..." onChange={(e)=>{setEmail(e.target.value)}}/>
+                            </td>
+                        </tr>
+                        <tr className="">
+                            <td className="py-2">Nom :</td>
+                            <td>
+                                <input className="h-[40px] w-[230px] rounded-lg bg-white/10 backdrop-blur-sm shadow-md outline-none px-3 text-[18px] font-thin" placeholder="Nom..." onChange={(e)=>{setFirstName(e.target.value)}}/>
+                            </td>
+                        </tr>
+                       <tr className="">
+                            <td className="py-2">Prénom :</td>
+                           <td>
+                                <input className="h-[40px] w-[230px] rounded-lg bg-white/10 shadow-md backdrop-blur-sm outline-none px-3 text-[18px] font-thin" placeholder="Prénom..." onChange={(e)=>{setLastName(e.target.value)}}/>
+                           </td>
+                        </tr>
+                        <tr className="">
+                            <td className="py-2 pr-12">Promotion :</td>
+                            <td>
+                                <input className="h-[40px] w-[230px] rounded-lg bg-white/10 shadow-md backdrop-blur-sm outline-none px-3 text-[18px] font-thin" placeholder="Promotion..." onChange={(e)=>{setPromotion(e.target.value)}}/>
+                            </td>
+                        </tr>
+                    </table>
+                    <button className="h-[40px] w-[120px] text-[20px] bg-blue-300 hover:bg-blue-400 rounded-full">Valider</button>
+                    <button className="absolute bottom-[35px] left-[20px]" onClick = {(e)=>{setOneClick(false);e.preventDefault()}}>
+                        <ArrowIcon/>
+                    </button>
+                </form>
+                <form className = {`h-[500px] w-[550px] bg-white/70 backdrop-blur-sm shadow-lg rounded-xl flex-col space-y-10 items-center justify-center relative text-[20px] text-center px-12 ${manyClick === true ? "flex" : "hidden"}`}>
+                    <div className="text-[30px]">Ajouter plusieurs étudiant</div>
+                    <div className="text-[17px] font-thin">Ajouter un ficher contenant plusieurs étudiant en cliquant sur le lien ci dessous.</div>
+                    <label className=" w-fit p-8 flex items-center justify-center flex-col cursor-pointer transition-transform ease-in bg-white/10 backdrop-blur-3xl rounded-3xl" for = "file">
+                        <div className=" flex justify-center items-center p-4 w-fit">         
+                        <label for='file'  className="flex space-x-2 group cursor-pointer">
+                                <AttachFileIcon className='w-6 text-[#5375E2]/80 group-hover:text-[#5375E2]/60'/>
+                                <div> Joindre des fichiers </div>
+                            </label>
+                        </div>
+                        <img src = {extension === "csv" ? "csv.jpg" : "excel.png"} className={`h-[80px] w-[80px] object-contain opacity-40 ${file ? "flex" : "hidden"}`}/>
+                        <div className="w-full text-center break-words text-sm">{file?.name}</div>
+                     </label>
+                     <input id="file" className="hidden" type="file" multiple onChange={(e) => {setFile(e.target.files[0])}} optional/>
+                    <button className="h-[40px] w-[120px] text-[18px] bg-blue-300 hover:bg-blue-400 rounded-full" type="submit">Valider</button>
+                    <button className="absolute bottom-[35px] left-[20px]" onClick = {(e)=>{setManyClick(false);e.preventDefault()}}>
+                        <ArrowIcon/>
+                    </button>
+                </form>
+            </div>
+       </div>
+    )
+}
+export default addStudent;
