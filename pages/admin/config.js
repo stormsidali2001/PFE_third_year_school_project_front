6d89cff535cfd67d7f58b8a9287b45fc9a593ffc
shAtud <sidalihouda.computerscience@gmail.com>
2022-05-15 19:14:01 +0100

    valider les équipes

--- pages/admin/config.js
+++ pages/admin/config.js
@@ -12,34 +12,31 @@ const config = props => {
     const [promotion , setPromotion] = useState(
         [
             {promo : ["2CPI"]} , 
-            {promo : ["1CS"]} ,
-            {promo : ["2CS"]},
-            {promo : ["3CS"]}]
+            {promo : ["1CS"]} , 
+            {promo : ["2CS"]} , 
+            {promo : ["3CS"]} , 
+        ]  
     )
-    const [specialite , setSpecialite] = useState(
-        [ 
-            {spécialité : ["SIW"]} , 
-            {spécialité : ["ISI"]} ,
-        ]
-    )
-    const [validerEquipes , setvaliderEquipe] = useState(true)
+   
+    const [validerEquipes , setvaliderEquipe] = useState(false)
     const [ValiderEquipePromotion , setValiderEquipePromotion] = useState(promotion)
-    const [ValiderEquipeSpécialité , setValiderEquipeSpécialité] = useState(specialite)
     const [downIconValiderEquipePromotion , setDownIconValiderEquipePromotion] = useState(false)
-    const [downIconValiderEquipeSpécialité , setDownIconValiderEquipeSpécialité] = useState(false)
     const [longueur , setLongueur] = useState(ValiderEquipePromotion.length)
-    const [longueurSpécialité , setLongueurSpécialité] = useState(ValiderEquipeSpécialité.length)
     const [choosenPromo , setChoosenPromo] = useState(false)
+    const [minEtudiantEquipe , setMinEtudiantEquipe] = useState(1)
+    const [maxEtudiantEquipe , setMaxEtudiantEquipe] = useState(1)
+    const [affecterEtudiantSansEquipes , setAffecterEtudiantsSansEquipes] = useState(false)
+    const [conpleterEquipes , setCompleterEquipes] = useState(false)
     return (
         <div>
             <StudentVerticalNavbar/>
             <HorisontalNavbar/>
             <div className="bg-background space-x-10 h-screen w-screen relative flex flex-row items-center justify-center font-xyz text-textcolor">
                 <div className="flex items-center justify-center relative flex-col text-[20px]">
-                    <div className="text-[35px] mt-16">Centre de contrôl :</div>
+                    <div className="text-[30px] absolute top-10">Centre de contrôl :</div>
                     <img src="/config.jpg" className={`h-[600px] object-contain mix-blend-darken ${validerEquipes === true ? "opacity-20":""}`}/>
                     <div className={`absolute h-[400px] w-[500px] bg-white/80 shadow-lg rounded-xl flex-col justify-center items-center flex ${validerEquipes === true ? "flex":"hidden"}`}>
-                        <div className="absolute top-4 text-[35px]">Valider les équipes :</div>
+                        <div className="absolute top-4 mb-2 text-[28px]">Valider les équipes :</div>
                         <div className={`flex flex-row space-x-4 items-start justify-center absolute top-24 ${choosenPromo === true ? "hidden" : "flex"}`}>
                             <div>Promotion :</div>
                             <div className="flex flex-col space-y-2">
@@ -58,27 +55,26 @@ const config = props => {
                                 </div>
                             </div>
                         </div>
-                        <div className={` ${choosenPromo === true ? "flex":"hidden"}`}>
-                        <div>Spécialité :</div>
-                            <div className="flex flex-col space-y-2">
-                               <div className="flex flex-row space-x-4">
-                                    <input className="h-[40px] w-[250px] rounded-full shadow-md flex items-center px-3" value = {`${longueurSpécialité === 1 ? ValiderEquipeSpécialité : "Choisir une promotion"}`} />
-                                    <button className={`pt-4 ${downIconValiderEquipeSpécialité === true ? "hidden": "flex"}`} onClick={(e)=>{setDownIconValiderEquipeSpécialité(true)}}><DownIcon/></button>
-                               </div>
-                                <div className={`bg-gray-50 shadow-sm rounded-xl p-2 backdrop-blur-sm justify-start w-[250px] flex flex-col ${downIconValiderEquipeSpécialité === true ? "flex": "hidden"}`}>
-                                {
-                                        specialite.map((element , index) => {
-                                            return (
-                                                <button className={`h-[35px] w-full hover:border-2 border-y-slate-200 border-x-transparent`} onClick={(e)=>{setValiderEquipeSpécialité(element.spécialité) ; setLongueurSpécialité(ValiderEquipeSpécialité.length) ; setDownIconValiderEquipeSpécialité(false)}}>{element.spécialité}</button>
-
-                                            )
-                                        })
-                                    }
+                        <div className={`items-center flex-col space-y-6 justify-center px-6 text-center ${choosenPromo === true ? "flex":"hidden"}`}>
+                            <div>Choisir le mon d'étudiants par équipe , la manière de traiter les étudiants sans équipes et les équipes non complètes</div>
+                            <div className="flex flex-row space-x-16">
+                                <div className="flex flex-row space-x-2">
+                                    <div>Min :</div>
+                                    <input className="h-[35px] w-[60px] bg-gray-50 shadow-md rounded-md px-3" type="number" min="1" max="10" value={minEtudiantEquipe} onChange = {(e) => setMinEtudiantEquipe(e.target.value)}/>
+                                </div>
+                                <div className="flex flex-row space-x-2">
+                                    <div>Max :</div>
+                                    <input className="h-[35px] w-[60px] bg-gray-50 shadow-md rounded-md px-3" type="number" min="2" max="10" value={maxEtudiantEquipe} onChange = {(e) => setMaxEtudiantEquipe(e.target.value)}/>
                                 </div>
                             </div>
+                            <div>
+                                <button className="hover:text-blue-400">Affecter les étudiants sans équipes</button>
+                                <button className="hover:text-blue-400">Compléter les équipes</button>
+                            </div>
+                            <button className="h-[40px] w-[120px] hover:bg-blue-500 bg-[#8FD4FB] rounded-full">Valider</button>
                         </div>
-                        <button onClick={(e) => {longueur === 1 ? setChoosenPromo(true) : ""}} className="rotate-180 absolute bottom-2 right-4"><ArrowIcon/></button>
-                        <button onClick={(e) => {longueur === 1 ? setChoosenPromo(false) : "hidden"}} className="absolute bottom-2 left-4"><ArrowIcon/></button>
+                        <button className={`rotate-180 absolute bottom-2 right-4 ${choosenPromo === true ? "hidden": "flex"}`} onClick={(e) => {longueur === 1 ? setChoosenPromo(true) : ""}}><ArrowIcon/></button>
+                        <button onClick={(e) => {longueur === 1 ? setChoosenPromo(false) : ""}} className={`absolute bottom-2 left-4 ${choosenPromo === false ? "hidden": "flex"}`} ><ArrowIcon/></button>
                     </div>
                 </div>
                 <div className="flex flex-col fit p-3 w-[350px] bg-white text-[20px] rounded-lg shadow-lg">
