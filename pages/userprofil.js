7c4b99a10bd861364ff388d9a73e66690e0f4839
shAtud <sidalihouda.computerscience@gmail.com>
2022-05-17 17:22:59 +0100

    user profil

--- pages/userprofil.js
+++ pages/userprofil.js
@@ -18,13 +18,31 @@ const userProfil = props => {
                 team : {
                     id : 1111,
                     nom : "jjjjjjjjjj",
-                    lienProfil : "/"
-                }
-            }
+                   
+                },
+            },
+            teacher:{
+                id : 1,
+                firstName : "yamina",
+                lastName : "bnadem",
+                pseudo : "Kado",
+                dob : "25/11/2042",
+            },
+            admin:{
+                id : 1,
+                firstName : "yamina",
+                lastName : "bnadem",
+                pseudo : "Kado",
+                dob : "25/11/2042",
+            },
+            
         }
 
         const [modifier , setModifier] = useState(false)
-        const [pseudo , setPseudo] = useState (data.student.pseudo)
+        const [typeUtilisateur , setTypeUtilisateur] = useState((data.userType === 'student' ? data.student : (data.userType === "teacher" ? data.teacher : data.admin)))
+        const [pseudo , setPseudo] = useState (typeUtilisateur.pseudo)
+        
+       
 
     return (
         <div>
@@ -33,51 +51,47 @@ const userProfil = props => {
             <div className="bg-background h-screen w-screen relative flex flex-col items-center space-y-16 font-xyz text-textcolor justify-center">
                 <img src="/studentProfil.jpg" className="h-[520px] object-contain mix-blend-darken opacity-30"/>
                 <div className="text-[20px] h-[500px] w-[500px] shadow-lg rounded-xl bg-white/50 backdrop-blur-sm flex items-center justify-center absolute">
-                {
-                    (data.userType === 'student' ?
-                        <div className="flex items-center justify-center flex-col space-y-6">
-                            <div className="text-[30px] flex flex-row space-x-4">{idUser === data.student.id ? "Votre Profil" : data.student.pseudo}</div>
-                           <div className="flex flex-col space-y-3">
+                {  
+                    <div className="flex items-center justify-center flex-col space-y-6">
+                        <div className="text-[30px] flex flex-row space-x-4">{idUser === data.student.id ? "Votre Profil" : data.student.pseudo}</div>
+                        <div className="flex flex-col space-y-3">
+                        <div className="flex flex-row space-x-4">
+                                <div>Nom :</div>
+                                <div>{typeUtilisateur.firstName}</div>
+                            </div>
                             <div className="flex flex-row space-x-4">
-                                    <div>Nom :</div>
-                                    <div>{data.student.firstName}</div>
-                                </div>
-                                <div className="flex flex-row space-x-4">
-                                    <div>Prénom :</div>
-                                    <div>{data.student.lastName}</div>
-                                </div>
-                                <div className="flex flex-row space-x-4">
-                                    <div>Pseudo :</div>
-                                    <div className={`${modifier === true ? "hidden" : "flex"}`}>{pseudo}</div>
-                                    <input
-                                        className={`${modifier === false ? "hidden" : "flex"}`}
-                                        value = {pseudo}
-                                        onChange = {(e) => setPseudo(e.target.value)}
-                                    />
-                                </div>
-                                <div className="flex flex-row space-x-4">
-                                    <div>Date of birth :</div>
-                                    <div>{data.student.dob}</div>
-                                </div>
-                                <div className="flex flex-row space-x-4">
-                                    <div>Email :</div>
-                                    <div>{data.email}</div>
-                                </div>
-                                <div className="flex flex-row space-x-4">
-                                    <div>Team :</div>
-                                    <div>{data.student.team.nom}</div>
-                                </div>
-                           </div>
-                            <button 
-                                className={`h-[35px] w-[130px] bg-blue-300 hover:bg-blue-400 rounded-full items-center justify-center ${idUser === data.student.id ? "flex" : "hidden"}`}
-                                onClick = {(e) => {setModifier(!modifier)}}
-                            >
-                                {modifier === false ? "Modifier" : "Valider"}
-                            </button>
+                                <div>Prénom :</div>
+                                <div>{typeUtilisateur.lastName}</div>
+                            </div>
+                            <div className="flex flex-row space-x-4">
+                                <div>Pseudo :</div>
+                                <div className={`${modifier === true ? "hidden" : "flex"}`}>{pseudo}</div>
+                                <input
+                                    className={`${modifier === false ? "hidden" : "flex"}`}
+                                    value = {pseudo}
+                                    onChange = {(e) => setPseudo(e.target.value)}
+                                />
+                            </div>
+                            <div className="flex flex-row space-x-4">
+                                <div>Date of birth :</div>
+                                <div>{typeUtilisateur.dob}</div>
+                            </div>
+                            <div className="flex flex-row space-x-4">
+                                <div>Email :</div>
+                                <div>{data.email}</div>
+                            </div>
+                            <div className={`flex flex-row space-x-4 ${data.userType === 'student' ? "flex" : "hidden"}`}>
+                                <div>Team :</div>
+                                <div>{data.student.team.nom}</div>
+                            </div>
                         </div>
-                    : 
-                        ""
-                    )
+                        <button 
+                            className={`h-[35px] w-[130px] bg-blue-300 hover:bg-blue-400 rounded-full items-center justify-center ${idUser === data.student.id ? "flex" : "hidden"}`}
+                            onClick = {(e) => {setModifier(!modifier)}}
+                        >
+                            {modifier === false ? "Modifier" : "Valider"}
+                        </button>
+                    </div>
                 }
                 </div>
             </div>
