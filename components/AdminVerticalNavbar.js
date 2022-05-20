f6ac03d8d0af64eadf1a2538dfde237fc497fb17
shAtud <sidalihouda.computerScience@gmail.com>
2022-05-20 15:38:18 +0200

    no capital letters on page names  please

--- components/AdminVerticalNavbar.js
+++ components/AdminVerticalNavbar.js
@@ -8,7 +8,7 @@ const AdminVerticalNavbar = props => {
         <div className="fixed h-[60vh] left-0 z-50 top-[50%] -translate-y-1/2  drop-shadow-[8px_8px_8px_rgba(0,0,0,0.25)]  flex flex-col justify-center space-y-8 border-2 bg-white rounded-tr-[100px]  rounded-br-[100px] w-[60px]">
             <div className='relative cursor-pointer group'>
                 <div className='absolute right-0 translate-x-[100%] top-0 text-textcolor bg-white rounded-80 font-semibold w-fit px-2 hidden group-hover:flex rounded-[10px] shadow-lg'>Dashboard</div>
-                <Link href="/adminDashboard">
+                <Link href="/admindashboard">
                     <button><Menu className='hover:cursor-pointer' /></button>
                 </Link>
             </div>
