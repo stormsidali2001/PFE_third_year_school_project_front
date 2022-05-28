c583b0edf39126f3429544412fa89783a1aa0a7e
shAtud <sidalihouda.computerScience@gmail.com>
2022-05-28 15:19:42 +0200

    myteams

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,16 +1,19 @@
 const Avatar = ({firstName , lastName , image}) => {
 
 
-    const colors = ["bg-orange-100 , bg-blue-200 , bg-lime-100 , bg-red-200 , bg-green-100 , bg-cyan-100"]
+    const colors = ["bg-orange-100" , "bg-yellow-100" , "bg-pink-200" , "bg-lime-100" , "bg-red-200" , "bg-green-100" , "bg-cyan-100"]
 
 
-    const chosenItem = colors[(colors.length-1)*Math.random()]
-
+    const chosenItem = colors[Math.floor((colors.length-1)*Math.random())]
+    
 
     return (
         <div>
             {
-                image !== null ? <img src = {image} className='h-[35px] w-[35px] object-contain rounded-full shadow-md'/> : <div className={`h-[35px] w-[35px] rounded-full shadow-md ${chosenItem}`}></div>
+                image !== null ? <img src = {image} className='h-[23px] w-[23px] object-contain rounded-full shadow-md'/> : <div className={`h-[23px] w-[23px] rounded-full shadow-md ${chosenItem} flex flex-row  text-[10px] items-center justify-center`}>
+                    <div className="uppercase">{firstName.charAt(0)}</div>
+                    <div>{lastName.charAt(0)}</div>
+                </div>
             }
         </div>
     )
--- pages/myteams/index.js
+++ pages/myteams/index.js
@@ -1,4 +1,8 @@
 import Avatar from "../../components/Avatar"
+import TeacherVerticalNavbar from "../../components/TeacherVerticalNavbar"
+import HorisontalNavbar from "../../components/HorisontalNavbar"
+import { useState } from "react"
+import  Select  from "react-select"
 
 const MyTeams = props => {
 
@@ -108,41 +112,95 @@ const MyTeams = props => {
                     id : 7,
                     firstName : 'etudiant',
                     lastName : 'etudiante',
+                    image : "/sondage.png"
+                },
+                {
+                    id : 6,
+                    firstName : 'membre',
+                    lastName : 'membre5',
                     image : null
+                },
+                {
+                    id : 7,
+                    firstName : 'etudiant',
+                    lastName : 'etudiante',
+                    image : "/sondage.png"
                 }
             ]
         }
     ]
 
+    const [choosenPromotion , setChoosenPromotion] = useState(null)
+
+    const promotions = [
+        {
+            id : 1,
+            name : '1CPI'
+        },
+        {
+            id : 1,
+            name : '2CPI'
+        },
+        {
+            id : 1,
+            name : '1CS'
+        },
+        {
+            id : 1,
+            name : '2CS'
+        },
+        {
+            id : 1,
+            name : '3CS'
+        }
+    ]
+
     return(
-        <div className="h-screen w-screen bg-background text-textcolor font-xyz flex items-center justify-center">
-            <div className="flex flex-row gap-16">
-                {
-                    teams.map ((el ,index) => {
-                        return(
-                            <div className="flex flex-col space-y-3 bg-gradient-to-b from-blue-50 to-blue-300 hover:bg-gradient-to-t h-[300px] w-[250px] rounded-lg border-2 border-slate-300 shadow-lg p-6 text-[17px] relative">
-                                <img src="/teamStudent.jpg" className="h-full object-contain mix-blend-darken opacity-20"/>
-                                <div className="absolute top-6">
-                                    <div className="w-full text-center text-[23px]">{el.name}</div>
-                                    {
-                                        el.members.map((element) => {
-                                            return (
-                                                <div className="flex flex-rox space-x-2">
-                                                    <Avatar firstName={element.firstName} lastName={element.lastName} image={element.image}/>
-                                                    <div>{element.firstName}</div>
-                                                    <div>{element.lastName}</div>
-                                                    {
-                                                        element.id === el.teamLeader.id ? <div className="underline">CF</div> : <div></div>
-                                                    }
-                                                </div>
-                                            )
-                                        })
-                                    }
+        <div>
+            
+            <div className="h-screen w-screen bg-background text-black font-xyz flex flex-col space-y-10 items-center justify-center">
+            <div className="flex flex-row space-x-4">
+                <div className='text-[20px]'>Choisir une promotion :</div>
+                <Select
+                    placeholder="Promotion..." 
+                    className="z-50 h-[40px] w-[230px] rounded-lg bg-slate-200 shadow-md backdrop-blur-sm outline-none  text-[18px] font-thin" 
+                    onChange={(option)=>{setChoosenPromotion(option)}}
+                    options={promotions.map(el=>{return {value:el.id,label:el.name}})}
+                    value={choosenPromotion}
+                    styles = {{menuPortal:base=>({...base,zIndex:500})}}
+                />
+            </div>
+                <div className="flex flex-row gap-16">
+                    {
+                        teams.map ((el ,index) => {
+                            return(
+                                <div className="flex flex-col space-y-3 bg-gradient-to-b from-blue-50 to-blue-100 hover:bg-gradient-to-t h-[300px] w-[250px] rounded-lg border-2 border-slate-300 shadow-lg p-6 text-[15px] relative">
+                                    <img src="/teamStudent.jpg" className="h-full object-contain mix-blend-darken opacity-20"/>
+                                    <div className="absolute top-2 flex flex-col space-y-2">
+                                        <div className="w-full text-center text-[17px]">{el.name}</div>
+                                        {
+                                            el.members.map((element) => {
+                                                return (
+                                                    <div className="flex flex-row items-center space-x-2">
+                                                        <Avatar firstName={element.firstName} lastName={element.lastName} image={element.image}/>
+                                                        <div className="text-black">{element.firstName}</div>
+                                                        <div className="text-black">{element.lastName}</div>
+                                                        {
+                                                            element.id === el.teamLeader.id ? <div className="underline text-black italic">CF</div> : <div></div>
+                                                        }
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                        <div className="w-full flex justify-center">
+                                            <button className="bg-blue-300 hover:bg-blue-400 rounded-full shadow-lg h-[30px] w-[100px]">Voir plus</button>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
     )
