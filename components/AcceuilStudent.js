1231e76a8697f8f54b3d8f88c2926f4b853eb8b6
shAtud <sidalihouda.computerScience@gmail.com>
2022-06-13 19:26:27 +0200

    Acceuil etudiant

--- components/AcceuilStudent.js
+++ components/AcceuilStudent.js
@@ -1,3 +1,4 @@
+import Link  from "next/link";
 
 const AcceuilStudent = props => {
     return (
@@ -7,8 +8,12 @@ const AcceuilStudent = props => {
                 <div className="text-[20px] px-6 text-center">Nous somme heureux de vous acceuil sur notre plateforme. On espère qu’elle vous serait utile dans le managment du votre projet,</div>
                 <div className="text-[22px] px-16 text-center">Cliquer sur l'un des boutons dessous et commencez votre expérience.</div>
                 <div className="text-[18px] flex flex-row space-x-6">
-                    <button className="bg-blue-200 rounded-full shadow-xl h-[35px] w-[150px] hover:bg-blue-300">Votre équipe</button>
-                    <button className="bg-blue-300 rounded-full shadow-xl h-[35px] w-[150px] hover:bg-blue-200">Chat</button>
+                    <Link href='/announcementlist'>
+                        <button className="bg-blue-200 rounded-full shadow-xl h-[35px] w-[150px] hover:bg-blue-300">Votre équipe</button>
+                    </Link>
+                    <Link href='/chat'>
+                        <button className="bg-blue-300 rounded-full shadow-xl h-[35px] w-[150px] hover:bg-blue-200">Chat</button>
+                    </Link>
                 </div>
             </div>
             <img src="AcceuilStudent.png" className="mix-blend-darken object-contain"/>
