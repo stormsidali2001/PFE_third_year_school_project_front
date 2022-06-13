50abd7adf13e94f016fb2c53a3e0dc09d847bf4b
shAtud <sidalihouda.computerScience@gmail.com>
2022-06-13 20:27:18 +0200

    Acceuil

--- components/Footer.js
+++ components/Footer.js
@@ -1,25 +1,17 @@
-import { Link } from "react-scroll/modules";
+import  Link  from "next/link";
 
 const Footer = props => {
     return(
-        <div className="h-fit w-[100vw] bg-[#8FD4FB]/70 flex md:flex-row flex-col lg:space-x-64 md:space-x-16 md:space-y-0 space-y-16 md:items-start md:justify-center justify-start pl-6 py-6 font-xyz text-textcolor font-light text-[20px]">
-            <div className="flex md:flex-row flex-col md:space-x-10">
-                <a>BP 73, Bureau de poste EL WIAM <br/> Sidi Bel Abbés 22016, Algérie</a>
-                <div className="flex flex-col">
-                    <Link><a>+213-48-74-94-52</a></Link>
-                    <Link><a>contact@esi-sba.dz</a></Link>
-                </div>
-            </div>
-            <div className="flex md:flex-row flex-col md:space-x-10">
-                <div className="flex flex-col">
-                    <Link to='home' spy={true} smooth={true}><a className="cursor-pointer">Home</a></Link>
-                    <Link to='service' spy={true} smooth={true}><a className="cursor-pointer">Services</a></Link>
-                </div>
-                <div className="flex flex-col">
-                    <Link to='what' spy={true} smooth={true}><a className="cursor-pointer">Quesque project101</a></Link>
-                    <Link to='why' spy={true} smooth={true}><a className="cursor-pointer">Pourquoi utiliser project101</a></Link>
-                </div>
-            </div>
+        <div className="w-[100vw] flex justify-center pb-3">
+            <div className="h-fit  text-center w-[100%] pb-2 flex flex-row space-x-[30%]  bg-[#8FD4FB]/50 pl-6 py-1 justify-center font-mono font-thin text-black  text-[18px]">
+            
+            <Link href="https://it-experts.vercel.app/">
+            <button className="text-[16px] flex flex-row space-x-1">
+                <div>&copy; Tous les droits réservé à</div>
+                <div className="underline hover:text-blue-600">IT-Expert 2022</div>
+            </button>
+            </Link>
+        </div>
         </div>
     )
 }
