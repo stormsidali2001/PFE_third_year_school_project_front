78ae269b34b76310fe25eee5e81bd0c8ea0bb98c
shAtud <sidalihouda.computerScience@gmail.com>
2022-06-13 18:57:46 +0200

    login

--- pages/login.js
+++ pages/login.js
@@ -62,36 +62,31 @@ const Login = ({toastsRef}) => {
     
     return(
         <div className="flex h-fit py-12 lg:py-0 lg:h-[100vh] w-[100vw] items-center justify-center" id="login">
-            <div className="w-[80%] flex lg:flex-row flex-col lg:space-x-56 space-y-8 lg:space-y-0 items-center justify-center">
-                <img src='Login.jpg' className="object-contain flex items-center justify-center"/>
-              
-                      
-    
-                       
-                  
+            <div className="max-w-[80%] flex lg:flex-row flex-col lg:space-x-56 space-y-8 lg:space-y-0 items-center justify-center">
+                <img src='Login.jpg' className="object-contain max-h-[70vh] flex items-center justify-center"/>
                         <form 
-                        className="flex flex-col space-y-6 w-[60vw]"
+                        className="flex flex-col gap-4 max-w-[60vw]"
                         onSubmit={handleSubmit}
                     >
                       
-                        <div className="text-[35px]">Bienvenu !</div>
-                        <div className="text-[25px]">E-mail :</div>
+                        <div className="text-[35px] font-semibold">Bienvenu !</div>
+                        <div className="text-[22px]">E-mail :</div>
                         <input 
                             placeholder="E-mail..."  
-                            className="h-[60px] lg:w-[360px] w-fit placeholder-[22px] text-[22px] outline-none border border-1 border-zinc-500 rounded-md px-6"
+                            className="h-[50px] lg:w-[100%] m-auto w-fit placeholder-[18px] text-[18px] outline-none border border-1 border-zinc-500 rounded-md px-4"
                             type='email'
                             onChange={(e)=>setEmail(e.target.value)}
                             value={email}
                         />
-                        <div className="text-[25px]">Mot de passe :</div>
+                        <div className="text-[22px]">Mot de passe :</div>
                         <input 
                             placeholder="Mot de passe..." 
                             type='password' 
-                            className="h-[60px] lg:w-[360px] w-fit placeholder-[22px] text-[22px] outline-none border border-1 border-zinc-500 rounded-md px-6"
+                            className="h-[50px] lg:w-[100%] m-auto w-fit placeholder-[18px] text-[18px] outline-none border border-1 border-zinc-500 rounded-md px-4"
                             onChange={(e)=>setPassword(e.target.value)}
                             value={password}
                         />
-                       <span className="text-cyan-700 text-[20px] hover:underline hover:underline-offset-1  hover:cursor-pointer    transition-all w-fit ease-in"> <Link href='/forgotpassword'>Mot de passe oublié ?</Link></span>
+                       <span className="text-cyan-700 text-[20px] hover:underline hover:underline-offset-1  hover:cursor-pointer  m-auto  transition-all text-center w-fit ease-in"> <Link href='/forgotpassword'>Mot de passe oublié ?</Link></span>
                        {
                             loading?(
                                 <svg role="status" class="h-[60px] lg:w-[360px] min-w-[250px] text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -99,13 +94,11 @@ const Login = ({toastsRef}) => {
     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
 </svg>
                             ):(
-                                <button disabled={loading} type='submit' className="hover:cursor-pointer text-[25px] bg-blue-600 rounded-md h-[60px] lg:w-[360px] min-w-[250px]">Se connecter</button>
+                                <button disabled={loading} type='submit' className="hover:cursor-pointer text-[17px] bg-blue-600 hover:bg-blue-600/80  max-w-[60%] m-auto text-center flex items-center justify-center rounded-md py-2 px-6 text-white/80">Connection</button>
                                 )
                        
                             }
                     </form>
-                   
-               
             </div>
         </div>
     )
